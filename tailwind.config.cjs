8d61031dd67ff52a2f42653984fc39dad5e2b135
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-06-23 13:36:48 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'tailblog': ['"Inter"', 'sans-serif'],
+			},
+			colors: {
+				'tailblog-blue': '#0096FF',
+				'tailblog-purple': '#40128B',
+				'tailblog-pink': '#D800A6',
+				'tailblog-orange': '#ff7849',
+				'tailblog-green': '#BCEB3C',
+				'tailblog-yellow': '#FFE79B',
+				'tailblog-grey-dark': '#212A3E',
+				'tailblog-grey': '#394867',
+				'tailblog-grey-light': '#DDE6ED',
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
+}
