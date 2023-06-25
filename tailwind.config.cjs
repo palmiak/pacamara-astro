877fd2acf69f64a70afa5c7599d01c4dafe3e0ca
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2023-06-25 22:52:47 +0200

    lista postów i config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,18 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			boxShadow: {
+				'tailblog-shadow': '0px 25px 50px -12px rgba(0, 0, 0, 0.3)',
+			},
 			fontFamily: {
 				'tailblog-inter': ['"Inter"', 'sans-serif'],
 				'tailblog-space': ['"Space Grotesk"', 'sans-serif'],
 			},
 			colors: {
-				'tailblog-primary': '#27374D',
-				'tailblog-secondary': '#9DB2BF',
-				'tailblog-accent': '#526D82',
-				'tailblog-dark': '#1B2736',
+				'tailblog-primary': '#4A4E69',
+				'tailblog-secondary': '#9A8C98',
+				'tailblog-accent': '#C9ADA7',
+				'tailblog-dark': '#22223B',
 			},
 		},
 	},
