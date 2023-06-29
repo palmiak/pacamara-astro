ed046d3f3df6347641084875cc5841f14b1b65ae
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2023-06-29 12:24:59 +0200

    new colors & gradients

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		backgroundSize: {
+			'gradient-dashed': '20px 2px, 100% 2px',
+		},
 		extend: {
 			boxShadow: {
 				'tailblog-shadow': '0px 25px 50px -12px rgba(0, 0, 0, 0.3)',
@@ -12,10 +15,11 @@ module.exports = {
 				'tailblog-space': ['"Space Grotesk"', 'sans-serif'],
 			},
 			colors: {
-				'tailblog-primary': '#001C30',
-				'tailblog-secondary': '#176B87',
-				'tailblog-accent': '#64CCC5',
-				'tailblog-dark': '#22223B',
+				'tailblog-primary': '#003049',
+				'tailblog-secondary': '#B2A4FF',
+				'tailblog-accent': '#FFB4B4',
+				'tailblog-dark': '#000E14',
+				'tailblog-white': '#ffffff',
 			},
 		},
 	},
