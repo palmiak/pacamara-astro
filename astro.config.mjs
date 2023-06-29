ec4b55ff507545987725b2ca8cfe4c31379186cc
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-06-29 17:48:55 +0200

    Optimization fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import alpine from '@astrojs/alpinejs';
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
@@ -9,5 +8,5 @@ export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), alpine(), mdx()]
+  integrations: [tailwind(), mdx()]
 });
