7cd94424b28b12c0e63e7b643b6b56080475b094
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-07-03 14:27:44 +0200

    og fix

--- src/config.mjs
+++ src/config.mjs
@@ -2,7 +2,7 @@ export default {
     title: "Homepage",
     titleSuffix: " | Pacamara ☕",
     description: "Aromatic brew, pure indulgence, exquisite satisfaction.Default description",
-    defaultOgImage: "./public/defaultog.jpg",
+    defaultOgImage: "./defaultog.jpg",
     latestPosts: 3,
     postsPerPage: 3,
     url: "https://domain.com",
