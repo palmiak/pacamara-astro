4bf8d90347ef22622b8ad3de7c0b9924bd0512c2
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-07-11 10:13:55 +0200

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import alpinejs from "@astrojs/alpinejs";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), mdx(), alpinejs()]
+  integrations: [tailwind(), mdx()]
 });
