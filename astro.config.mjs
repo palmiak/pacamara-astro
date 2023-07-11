689e9a860873162abef0e71cecc727c775d7d773
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-07-11 10:13:46 +0200

    hamburger menu beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), alpinejs()]
 });
