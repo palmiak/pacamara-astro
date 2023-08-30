2255d958e73f059c6621c87ea3d6da9cac00cfed
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-08-30 22:53:22 +0200

    Astro 3.0 compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
   integrations: [tailwind(), mdx()],
   site: "https://pacamara-astro-6y7xr.kinsta.page"
 });
